f901585d1927bb975c046fc6ef238e81bbe18133
3nd3r1 <ender.spam.guru@gmail.com>
2024-03-12 14:49:07 +0200

    fix in remap

--- lua/ender/remap.lua
+++ lua/ender/remap.lua
@@ -7,4 +7,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set("n", "<leader>f", require('conform').format())
+vim.keymap.set("n", "<leader>f", require('conform').format)
+
+
+
