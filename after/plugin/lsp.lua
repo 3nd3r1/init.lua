fb7ae22a012f2b4bf8ce84b24c71ffc4e3cce70d
3nd3r1 <ender.spam.guru@gmail.com>
2024-04-17 08:58:54 +0300

    lsp: fix formatters
    Change conform formatters to correct ones.
    Format some lua files.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,73 +1,73 @@
-local lsp = require('lsp-zero')
+local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
 cmp.setup({
-    mapping = cmp.mapping.preset.insert({
-        -- `Enter` key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+	mapping = cmp.mapping.preset.insert({
+		-- `Enter` key to confirm completion
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-        -- Ctrl+Space to trigger completion menu
-        ['<C-Space>'] = cmp.mapping.complete(),
+		-- Ctrl+Space to trigger completion menu
+		["<C-Space>"] = cmp.mapping.complete(),
 
-        -- Navigate between completions
-        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.select }),
-        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.select }),
-    })
+		-- Navigate between completions
+		["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.select }),
+		["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.select }),
+	}),
 })
 
 lsp.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    client.server_capabilities.semanticTokensProvider = nil;
-    lsp.default_keymaps({ buffer = bufnr });
-    if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
-      vim.diagnostic.disable() 
-    end
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	client.server_capabilities.semanticTokensProvider = nil
+	lsp.default_keymaps({ buffer = bufnr })
+	if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
+		vim.diagnostic.disable()
+	end
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        "lua_ls",
-        "pylsp",
-        "tsserver",
-        "html",
-        "yamlls",
-        "helm_ls",
-    },
-    handlers = {
-        lsp.default_setup,
-        lua_ls = function()
-            local lua_opts = lsp.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-        end,
-        helm_ls = function()
-            require('lspconfig').helm_ls.setup({
-                settings = {
-                    ['helm-ls'] = {
-                        yamlls = {
-                            path = "yaml-language-server",
-                        }
-                    }
-                }
-            })
-        end,
-    }
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"pylsp",
+		"tsserver",
+		"html",
+		"yamlls",
+		"helm_ls",
+	},
+	handlers = {
+		lsp.default_setup,
+		lua_ls = function()
+			local lua_opts = lsp.nvim_lua_ls()
+			require("lspconfig").lua_ls.setup(lua_opts)
+		end,
+		helm_ls = function()
+			require("lspconfig").helm_ls.setup({
+				settings = {
+					["helm-ls"] = {
+						yamlls = {
+							path = "yaml-language-server",
+						},
+					},
+				},
+			})
+		end,
+	},
 })
 
 -- Formatter
-local conform = require('conform')
+local conform = require("conform")
 conform.setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- Use a sub-list to run only the first available formatter
-        javascript = { { "prettierd", "prettier" } },
-        yaml = { "prettierd" },
-    },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		javascript = { { "prettierd", "prettier" } },
+		typescript = { { "prettierd", "prettier" } },
+		html = { { "prettierd", "prettier" } },
+		yaml = { { "prettierd", "prettier" } },
+	},
 })
--- lua/ender/packer.lua
+++ lua/ender/packer.lua
@@ -1,75 +1,73 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
--- Packer can manage itself
-use 'wbthomason/packer.nvim'
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-}
+	-- Colorscheme
+	-- use({
+	--    'Shatur/neovim-ayu',
+	--    as = 'ayu',
+	--    config = function()
+	--        vim.cmd('colorscheme ayu');
+	--    end
+	-- })
 
--- Colorscheme
--- use({
---    'Shatur/neovim-ayu',
---    as = 'ayu',
---    config = function()
---        vim.cmd('colorscheme ayu');
---    end
--- })
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use("rcarriga/nvim-notify")
+	use("nvim-tree/nvim-web-devicons")
+	use("lambdalisue/suda.vim")
 
-use('theprimeagen/harpoon')
-use('mbbill/undotree')
-use('tpope/vim-fugitive')
-use('rcarriga/nvim-notify')
-use('nvim-tree/nvim-web-devicons')
-use('lambdalisue/suda.vim')
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
+	use({
+		"goolord/alpha-nvim",
+		requires = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	})
 
-use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
-use {
-    'goolord/alpha-nvim',
-    requires = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
-}
+	-- LSP STUFF -----------------------------------------------------------------------------------------------------
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
--- LSP STUFF -----------------------------------------------------------------------------------------------------
-use('nvim-treesitter/nvim-treesitter', {run =':TSUpdate'})
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			--- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
-    --- Uncomment the two plugins below if you want to manage the language servers from neovim
-    --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "towolf/vim-helm", ft = "helm" },
 
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    {'towolf/vim-helm', ft = 'helm'},
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
+			-- formatting
+			"stevearc/conform.nvim",
 
-    -- formatting
-   'stevearc/conform.nvim',
-
-    -- copilot
-    'github/copilot.vim',
-
-  }
-}
-
+			-- copilot
+			"github/copilot.vim",
+		},
+	})
 end)
