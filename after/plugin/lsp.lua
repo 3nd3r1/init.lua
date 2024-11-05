a84e16a9e7d51172d0858a81ffd4a39f2aea1c6b
3nd3r1 <ender.spam.guru@gmail.com>
2024-11-05 13:04:46 +0200

    lsp: fix typo

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -151,7 +151,7 @@ require("mason-lspconfig").setup({
                     local util = require("lspconfig").util
                     local root = util.root_pattern("package.json")(fname)
                     -- If there deno.json and package.json, its deno project
-                    if root and not util.path.is_fule(root .. "/deno.json") then
+                    if root and not util.path.is_file(root .. "/deno.json") then
                         return root
                     end
                     return nil
